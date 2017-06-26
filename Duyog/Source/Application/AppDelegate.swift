11e596504d3fea8fd1e6f5c2870e1c3ac939a927
Mounir Ybanez <rinuom91@gmail.com>
2017-06-26 10:43:50 +0800

    Initial commit.

---
+++ Duyog/Source/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Duyog
+//
+//  Created by Mounir Ybanez on 26/06/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Duyog/Source/Modules/SignIn/SignInViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.swift
+//  Duyog
+//
+//  Created by Mounir Ybanez on 26/06/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+}
