17f497afb0d7d19feb5834e811101a4121b7a3e3
Mounir Ybanez <rinuom91@gmail.com>
2017-06-26 15:08:01 +0800

    Added GradientView and UITheme.

---
+++ Duyog/Source/Helpers/GradientView.swift
@@ -0,0 +1,76 @@
+//
+//  GradientView.swift
+//  Duyog
+//
+//  Created by Mounir Ybanez on 26/06/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+// Reference: https://medium.com/ios-os-x-development/swift-3-easy-gradients-54ccc9284ce4
+
+import UIKit
+
+typealias GradientType = (x: CGPoint, y: CGPoint)
+
+enum GradientPoint {
+    case leftRight
+    case rightLeft
+    case topBottom
+    case bottomTop
+    case topLeftBottomRight
+    case bottomRightTopLeft
+    case topRightBottomLeft
+    case bottomLeftTopRight
+    
+    func draw() -> GradientType {
+        switch self {
+        case .leftRight:
+            return (x: CGPoint(x: 0, y: 0.5), y: CGPoint(x: 1, y: 0.5))
+        case .rightLeft:
+            return (x: CGPoint(x: 1, y: 0.5), y: CGPoint(x: 0, y: 0.5))
+        case .topBottom:
+            return (x: CGPoint(x: 0.5, y: 0), y: CGPoint(x: 0.5, y: 1))
+        case .bottomTop:
+            return (x: CGPoint(x: 0.5, y: 1), y: CGPoint(x: 0.5, y: 0))
+        case .topLeftBottomRight:
+            return (x: CGPoint(x: 0, y: 0), y: CGPoint(x: 1, y: 1))
+        case .bottomRightTopLeft:
+            return (x: CGPoint(x: 1, y: 1), y: CGPoint(x: 0, y: 0))
+        case .topRightBottomLeft:
+            return (x: CGPoint(x: 1, y: 0), y: CGPoint(x: 0, y: 1))
+        case .bottomLeftTopRight:
+            return (x: CGPoint(x: 0, y: 1), y: CGPoint(x: 1, y: 0))
+        }
+    }
+}
+
+class GradientLayer : CAGradientLayer {
+    var gradient: GradientType? {
+        didSet {
+            startPoint = gradient?.x ?? CGPoint.zero
+            endPoint = gradient?.y ?? CGPoint.zero
+        }
+    }
+}
+
+class GradientView: UIView {
+    override public class var layerClass: Swift.AnyClass {
+        get {
+            return GradientLayer.self
+        }
+    }
+}
+
+protocol GradientViewProvider {
+    associatedtype GradientViewType
+}
+
+extension GradientViewProvider where Self: UIView, Self.GradientViewType: CAGradientLayer {
+    var gradientLayer: Self.GradientViewType {
+        return layer as! Self.GradientViewType
+    }
+}
+
+extension UIView: GradientViewProvider {
+    typealias GradientViewType = GradientLayer
+}
---
+++ Duyog/Source/Helpers/UITheme.swift
@@ -0,0 +1,62 @@
+//
+//  UITheme.swift
+//  Duyog
+//
+//  Created by Mounir Ybanez on 26/06/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+import UIKit
+
+struct UITheme {
+    
+    lazy private(set) var font: UIThemeFont = {
+        return UIThemeFont()
+    }()
+    
+    lazy private(set) var color: UIThemeColor = {
+        return UIThemeColor()
+    }()
+}
+
+struct UIThemeFont {
+    
+    func regular(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "OpenSans", size: size)!
+    }
+    
+    func medium(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "OpenSans-Semibold", size: size)!
+    }
+    
+    func bold(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "OpenSans-Bold", size: size)!
+    }
+    
+    func light(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "OpenSans-Light", size: size)!
+    }
+    
+    func custom(_ name: String, _ size: CGFloat) -> UIFont? {
+        return UIFont(name: name, size: size)
+    }
+}
+
+struct UIThemeColor {
+    
+    var black: UIColor {
+        return UIColor(red: 27/255, green: 33/255, blue: 45/255, alpha: 1)
+    }
+    
+    var pink: UIColor {
+        return UIColor(red: 241/255, green: 59/255, blue: 111/255, alpha: 1)
+    }
+    
+    var violet: UIColor {
+        return UIColor(red: 103/255, green: 56/255, blue: 155/255, alpha: 1)
+    }
+    
+    var gray: UIColor {
+        return UIColor(red: 123/255, green: 133/255, blue: 154/255, alpha: 1)
+    }
+}
