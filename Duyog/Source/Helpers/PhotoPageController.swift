227c70a67843ee7a54de4ec82b6c7bf2e4070169
Mounir Ybanez <rinuom91@gmail.com>
2017-07-03 16:23:29 +0800

    Refactored PhotoPageController.
    - Added delegate and dataSource
    - Removed pageSources that could lead to a massive number of view controller instances

--- Duyog/Source/Helpers/PhotoPageController.swift
+++ Duyog/Source/Helpers/PhotoPageController.swift
@@ -8,150 +8,233 @@
 
 import UIKit
 
-class PhotoPageController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+protocol PhotoPageControllerDataSource: class {
     
-    var sources: [PhotoPageSource] = [] {
-        didSet {
-            guard isViewLoaded else { return }
-            
-            createPages()
-            selectPage(currentIndex, animated: false)
-        }
+    func photoPageControllerPhotoCount(_ controller: PhotoPageController) -> Int
+    func photoPageController(_ controller: PhotoPageController, assetFor index: Int) -> PhotoPageAsset
+}
+
+protocol PhotoPageControllerDelegate: class {
+    
+    func photoPageController(_ controller: PhotoPageController, configure imageView: UIImageView)
+    func photoPageController(_ controller: PhotoPageController, didChoose index: Int)
+    func photoPageController(_ controller: PhotoPageController, computePageSizeRelativeTo referenceSize: CGSize) -> CGSize
+    func photoPageController(_ controller: PhotoPageController, computeLineSpacingRelativeTo horizontalEdgeInset: CGFloat) -> CGFloat
+}
+
+extension PhotoPageControllerDelegate {
+    
+    func photoPageController(_ controller: PhotoPageController, didChoose index: Int) { }
+    
+    func photoPageController(_ controller: PhotoPageController, computePageSizeRelativeTo referenceSize: CGSize) -> CGSize {
+        var size = CGSize.zero
+        size.width = min(referenceSize.width, referenceSize.height)
+        size.height = size.width
+        return size
     }
     
-    var pages: [PhotoPage] = []
-    var pendingIndex: Int = 0
-    var currentIndex: Int = 0
-    
-    convenience init() {
-        self.init(transitionStyle: .scroll, navigationOrientation: .horizontal)
-    }
-    
-    override func loadView() {
-        super.loadView()
-        
-        view.backgroundColor = .clear
-        
-        dataSource = self
-        delegate = self
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        createPages()
-        selectPage(currentIndex, animated: false)
-    }
-    
-    func selectPage(_ index: Int, animated: Bool = true) {
-        guard pages.count > 0, index >= 0, index < pages.count else { return }
-        
-        var direction: UIPageViewControllerNavigationDirection = .forward
-        if index < currentIndex {
-            direction = .reverse
-        }
-        
-        setViewControllers([pages[index]], direction: direction, animated: animated, completion: nil)
-    }
-    
-    func createPages() {
-        guard sources.count > 0 else { return }
-        
-        pages = sources.map({ source -> PhotoPage in
-            let page = PhotoPage()
-            page.source = source
-            return page
-        })
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let index = pageIndex(of: viewController), index < pages.count - 1 else { return nil }
-        
-        return pages[index + 1]
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let index = pageIndex(of: viewController), index > 0 else { return nil }
-        
-        return pages[index - 1]
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
-        guard pendingViewControllers.count > 0,
-            let vc = pendingViewControllers.first as? PhotoPage,
-            let index = pageIndex(of: vc) else {
-            return
-        }
-        
-        pendingIndex = index
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-        guard completed else { return }
-        
-        currentIndex = pendingIndex
-    }
-    
-    func pageIndex(of viewController: UIViewController) -> Int? {
-        guard let page = viewController as? PhotoPage,
-            let index = pages.index(of: page) else {
-            return nil
-        }
-        
-        return index
+    func photoPageController(_ controller: PhotoPageController, computeLineSpacingRelativeTo horizontalEdgeInset: CGFloat) -> CGFloat {
+        return horizontalEdgeInset * 0.2
     }
 }
 
-class PhotoPage: UIViewController {
+class PhotoPageController: UIViewController {
     
-    var imageView: UIImageView!
-    var source: PhotoPageSource = .image(nil)
+    var collectionView: UICollectionView!
+    var flowLayout: UICollectionViewFlowLayout!
+
+    weak var dataSource: PhotoPageControllerDataSource!
+    weak var delegate: PhotoPageControllerDelegate!
     
     override func loadView() {
         super.loadView()
         
-        var theme = UITheme()
+        flowLayout = CenterCellCollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
         
-        imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
-        imageView.backgroundColor = theme.color.gray
-        imageView.layer.cornerRadius = 3
-        imageView.layer.masksToBounds = true
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.alwaysBounceHorizontal = true
+        collectionView.decelerationRate = UIScrollViewDecelerationRateFast
         
-        view.addSubview(imageView)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        PhotoPageCell.register(in: collectionView)
         
-        switch source {
-        case .bundle(let name):
-            imageView.image = UIImage(named: name)
-        
-        case .image(let image):
-            imageView.image = image
-        
-        default:
-            break
-        }
+        view.addSubview(collectionView)
     }
     
     override func viewDidLayoutSubviews() {
         var rect = CGRect.zero
         
-        let dimension = min(view.frame.width, view.frame.height)
-        rect.size.width = dimension
-        rect.size.height = rect.width
-        rect.origin.x = (view.frame.width - rect.width) / 2
-        rect.origin.y = (view.frame.height - rect.height) / 2
+        rect.size = view.frame.size
+        collectionView.frame = rect
+        
+        flowLayout.itemSize = delegate.photoPageController(self, computePageSizeRelativeTo: rect.size)
+        
+        // Reference [1]
+        var insets = collectionView.contentInset
+        let horizonalEdgeInset = (collectionView.frame.width - flowLayout.itemSize.width) * 0.5
+        insets.left = horizonalEdgeInset
+        insets.right = horizonalEdgeInset
+        collectionView.contentInset = insets
+        
+        flowLayout.minimumLineSpacing = delegate.photoPageController(self, computeLineSpacingRelativeTo: horizonalEdgeInset)
+        
+        collectionView.reloadData()
+    }
+}
+
+extension PhotoPageController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataSource?.photoPageControllerPhotoCount(self) ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = PhotoPageCell.dequeue(from: collectionView, at: indexPath)
+        cell.configure(dataSource.photoPageController(self, assetFor: indexPath.row))
+        delegate.photoPageController(self, configure: cell.imageView)
+        return cell
+    }
+}
+
+extension PhotoPageController: UICollectionViewDelegateFlowLayout {
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let point = CGPoint(
+            x: scrollView.center.x + scrollView.contentOffset.x,
+            y: scrollView.center.y + scrollView.contentOffset.y
+        )
+        let indexPath = collectionView.indexPathForItem(at: point)
+        
+        guard indexPath != nil else { return }
+        
+        delegate.photoPageController(self, didChoose: indexPath!.row)
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        guard !decelerate else { return }
+        
+        scrollViewDidEndDecelerating(scrollView)
+    }
+}
+
+class PhotoPageCell: UICollectionViewCell {
+    
+    var imageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        initSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initSetup()
+    }
+    
+    func initSetup() {
+        imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        
+        addSubview(imageView)
+    }
+    
+    override func layoutSubviews() {
+        var rect = CGRect.zero
+        
+        rect.size = frame.size
         imageView.frame = rect
     }
 }
 
-enum PhotoPageSource {
+extension PhotoPageCell: CollectionViewReusableProtocol {
     
-    case local(String)
+    typealias Cell = PhotoPageCell
+}
+
+enum PhotoPageAsset {
+    
     case bundle(String)
     case remote(String)
+    case local(String)
     case image(UIImage?)
 }
+
+protocol PhotoPageConfig {
+    
+    func configure(_ asset: PhotoPageAsset)
+}
+
+extension PhotoPageCell: PhotoPageConfig {
+    
+    func configure(_ asset: PhotoPageAsset) {
+        switch asset {
+        case .image(let image):
+            imageView.image = image
+            
+        case .bundle(let imageName):
+            imageView.image = UIImage(named: imageName)
+            
+        default:
+            break
+        }
+        
+        setNeedsLayout()
+        layoutIfNeeded()
+    }
+}
+
+// Reference [1]
+class CenterCellCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    
+    var mostRecentOffset : CGPoint = CGPoint()
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        if velocity.x == 0 {
+            return mostRecentOffset
+        }
+        
+        if let cv = self.collectionView {
+            let cvBounds = cv.bounds
+            let halfWidth = cvBounds.size.width * 0.5;
+            
+            if let attributesForVisibleCells = self.layoutAttributesForElements(in: cvBounds) {
+                var candidateAttributes : UICollectionViewLayoutAttributes?
+                for attributes in attributesForVisibleCells {
+                    // == Skip comparison with non-cell items (headers and footers) == //
+                    if attributes.representedElementCategory != UICollectionElementCategory.cell {
+                        continue
+                    }
+                    
+                    if (attributes.center.x == 0) || (attributes.center.x > (cv.contentOffset.x + halfWidth) && velocity.x < 0) {
+                        continue
+                    }
+                    candidateAttributes = attributes
+                }
+                
+                // Beautification step , I don't know why it works!
+                if(proposedContentOffset.x == -(cv.contentInset.left)) {
+                    return proposedContentOffset
+                }
+                
+                guard let _ = candidateAttributes else {
+                    return mostRecentOffset
+                }
+                
+                mostRecentOffset = CGPoint(x: floor(candidateAttributes!.center.x - halfWidth), y: proposedContentOffset.y)
+                return mostRecentOffset
+            }
+        }
+        
+        // fallback
+        mostRecentOffset = super.targetContentOffset(forProposedContentOffset: proposedContentOffset)
+        return mostRecentOffset
+    }
+}
+
+// Reference
+// [1] http://blog.karmadust.com/centered-paging-with-preview-cells-on-uicollectionview/
