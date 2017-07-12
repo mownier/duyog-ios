90731f9f007a99100a93d321a6911dd4c750afc6
Mounir Ybanez <rinuom91@gmail.com>
2017-07-12 14:27:22 +0800

    Added showing of Initial module.

--- Duyog/Source/FlowController/FlowModule.swift
+++ Duyog/Source/FlowController/FlowModule.swift
@@ -17,11 +17,13 @@ enum FlowModule: FlowModuleProtocol {
     
     case songList
     case musicPlayer
+    case initial
     
     var id: String {
         switch self {
         case .songList: return "SongList"
         case .musicPlayer: return "MusicPlayer"
+        case .initial: return "Initial"
         }
     }
 }
@@ -38,6 +40,12 @@ extension FlowControllerProtocol {
         let assembly = MusicPlayerAssembly(generator: generator)
         let child = assembly.assemble(songs: songs, moduleOutput: moduleOutput)
         enter(state, module: FlowModule.musicPlayer, viewController: child)
+    }
+    
+    func showInitial(_ state: FlowState, moduleOutput: InitialModuleOutputProtocol? = nil, generator: InitialViewControllerGeneratorProtocol = InitialViewControllerGenerator()) {
+        let assembly = InitialAssembly(generator: generator)
+        let child = assembly.assemble(moduleOutput: moduleOutput)
+        enter(state, module: FlowModule.initial, viewController: child)
     }
 }
 
