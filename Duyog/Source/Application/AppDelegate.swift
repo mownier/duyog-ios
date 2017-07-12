705acaa29811fd51c88118971d0770e969c0c2e8
Mounir Ybanez <rinuom91@gmail.com>
2017-07-12 12:33:03 +0800

    Checkpoint: Set music player as root.

--- Duyog/Source/Application/AppDelegate.swift
+++ Duyog/Source/Application/AppDelegate.swift
@@ -12,11 +12,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var flowController: FlowController!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let navRoot = InitialViewController()
-        let nav = createNavigationController(navRoot)
-        window?.rootViewController = nav
+        let song = Song(id: "s1", title: "Pero Atik Ra", genre: "Vispop", duration: 180, streamURL: "")
+        let artist = Artist(id: "a1", name: "Jacky Chang", bio: "", genre: "Pop")
+        let album = Album(id: "b1", photoURL: "", name: "Vispop", year: 2017)
+        let data = Song.Data(song: song, artists: [artist], albums: [album])
+        flowController = FlowController(window: window!)
+        flowController.showMusicPlayer(.root(nav), songs: [data, data, data, data, data, data], moduleOutput: nil)
         return true
     }
 }
@@ -25,6 +29,15 @@ func createNavigationController(_ root: UIViewController) -> UINavigationControl
     let nav = UINavigationController(rootViewController: root)
     nav.isNavigationBarHidden = true
     nav.navigationBar.isTranslucent = false
+    return nav
+}
+
+var nav: UINavigationController {
+    let nav = UINavigationController()
+    nav.navigationBar.setBackgroundImage(UIImage(), for: .default)
+    nav.navigationBar.shadowImage = UIImage()
+    nav.navigationBar.isTranslucent = true
+    nav.navigationBar.tintColor = UIColor.white
     return nav
 }
 
