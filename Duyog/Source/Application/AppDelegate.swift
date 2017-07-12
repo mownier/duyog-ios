193efa307017066ae19653e10eea96bc9af9e054
Mounir Ybanez <rinuom91@gmail.com>
2017-07-12 14:28:15 +0800

    Let Initial module as window root.

--- Duyog/Source/Application/AppDelegate.swift
+++ Duyog/Source/Application/AppDelegate.swift
@@ -15,24 +15,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var flowController: FlowController!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let song = Song(id: "s1", title: "Pero Atik Ra", genre: "Vispop", duration: 180, streamURL: "")
-        let artist = Artist(id: "a1", name: "Jacky Chang", bio: "", genre: "Pop")
-        let album = Album(id: "b1", photoURL: "", name: "Vispop", year: 2017)
-        let data = Song.Data(song: song, artists: [artist], albums: [album])
         flowController = FlowController(window: window!)
-        flowController.showMusicPlayer(.root(nav), songs: [data, data, data, data, data, data], moduleOutput: nil)
+        showInitialAsRoot(flowController)
         return true
     }
 }
 
-func createNavigationController(_ root: UIViewController) -> UINavigationController {
-    let nav = UINavigationController(rootViewController: root)
-    nav.isNavigationBarHidden = true
-    nav.navigationBar.isTranslucent = false
-    return nav
+func showMusicPlayerAsRoot(_ flowController: FlowControllerProtocol) {
+    let song = Song(id: "s1", title: "Pero Atik Ra", genre: "Vispop", duration: 180, streamURL: "")
+    let artist = Artist(id: "a1", name: "Jacky Chang", bio: "", genre: "Pop")
+    let album = Album(id: "b1", photoURL: "", name: "Vispop", year: 2017)
+    let data = Song.Data(song: song, artists: [artist], albums: [album])
+    flowController.showMusicPlayer(.root(navigationController), songs: [data, data, data, data, data, data], moduleOutput: nil)
 }
 
-var nav: UINavigationController {
+func showInitialAsRoot(_ flowController: FlowControllerProtocol) {
+    let nav = navigationController
+    nav.isNavigationBarHidden = true
+    flowController.showInitial(.root(nav))
+}
+
+var navigationController: UINavigationController {
     let nav = UINavigationController()
     nav.navigationBar.setBackgroundImage(UIImage(), for: .default)
     nav.navigationBar.shadowImage = UIImage()
